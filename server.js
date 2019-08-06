6ce4e8c22a5a75c8cda2fdc452e26270ccb0f40b
NChislom <11775893+nchislom@users.noreply.github.com>
2019-08-06 18:22:07 -0400

    Added mongoose connection URI and Heroku deployment support (notes in server.js)

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+/* Notes on Heroku deployment
+* In order to deploy this project to Heroku, we must set up an mLab provision.
+* mLab is remote MongoDB database that Heroku supports natively.
+* 
+* To get it running:
+* 
+* 1. Create a Heroku app in your project directory.
+* 2. Run this command in your Terminal/Bash window:
+* `heroku addons:create mongolab`
+* This command will add the free mLab provision to your project.
+*/
+
+// If deployed, use the deployed database. Otherwise use the local storydb database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/storydb";
+mongoose.connect(MONGODB_URI);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
