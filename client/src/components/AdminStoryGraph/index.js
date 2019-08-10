d4e1ce71be020e746c81ebad484584f8edbdd705
NChislom <11775893+nchislom@users.noreply.github.com>
2019-08-10 13:03:17 -0400

    Added graph div admin component

---
+++ client/src/components/AdminStoryGraph/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const AdminStoryGraph = (props) => {
+  return (
+    <div id={ props.id }>
+    </div>
+  );
+};
+
+export default AdminStoryGraph;
