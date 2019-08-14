bd48fa9ca1eccbe8b6d73a216a63bb64c3e44d94
Trash Panda <d.george.schlosser@gmail.com>
2019-08-13 20:14:06 -0400

    add non-functional buttons into story component

--- client/src/components/Story/index.js
+++ client/src/components/Story/index.js
@@ -20,8 +20,11 @@ class Story extends Component {
     return (
       <div className="Story">
         {this.state.data ? this.state.data.map(story => (
-          <h4>{story.scene_text}</h4>
-        
+          <div>
+            <h4>{story.scene_text}</h4>
+            <button>{story.choice_a}</button>
+            <button>{story.choice_b}</button>
+          </div>
         )) : <p>No Data :(</p>
         }
         
