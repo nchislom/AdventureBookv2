f64680c598877c588a704e2206e30e67ccfa6678
NChislom <11775893+nchislom@users.noreply.github.com>
2019-08-15 19:14:35 -0400

    refactor to stateful component; tested ok

--- client/src/components/Story/index.js
+++ client/src/components/Story/index.js
@@ -3,10 +3,11 @@ import API from "../../utils/API";
 
 
 class Story extends Component {
-
-  state = {
-    data : []
-  }
+  
+    state = {
+      data : [],
+      dataIndex: 0
+    }
 
   componentDidMount() {
     API.getFullStory().then(res => {
@@ -14,23 +15,32 @@ class Story extends Component {
         data: res.data
       },()=>console.log(this.state.data))
     })
-  }
+  };
+
+  // this.props = {
+  //   id: story.id,
+  //   crntScene: story.scene_title,
+  //   text: story.scene_text,
+  //   nxtScene: story.next_scene,
+  //   crctChoice: story.correct_choice,
+  //   choice_a: story.choice_a,
+  //   choice_b: story.choice_b,
+  //   wrongChoiceText: story.wrong_choice_result
+  // }
 
   render() {
+    let story = this.state.data[this.state.dataIndex];
+
     return (
-      <div className="Story">
-        {this.state.data ? this.state.data.map(story => (
-          <div>
-            <h4>{story.scene_text}</h4>
-            <button>{story.choice_a}</button>
-            <button>{story.choice_b}</button>
-          </div>
-        )) : <p>No Data :(</p>
-        }
+      story ?
+        <div className="story">
+          <h4>{story.scene_text}</h4>
+          <button>{story.choice_a}</button>
+          <button>{story.choice_b}</button>
+        </div>
         
-        
-      </div>
-    );
+        : <div></div>
+    )
   }
 }
 
