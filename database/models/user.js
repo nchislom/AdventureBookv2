44f9b0e6bc9796ce02442c8f1baf27cd674c6421
NChislom <11775893+nchislom@users.noreply.github.com>
2019-08-15 17:50:05 -0400

    replacing missing user model

---
+++ database/models/user.js
@@ -0,0 +1,19 @@
+// /database/user.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure 
+const UserSchema = new Schema(
+  {
+    userName: String,
+    firstName: String,
+    lastName: String,
+    password: String,
+    bookmark: Number
+    // image: String
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("User", UserSchema);
